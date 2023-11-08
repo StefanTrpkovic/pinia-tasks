5bee86b592bdf3ec588e328b4e18845e718e6a70
Stefan Trpkovic Sentice <stefan.trpkovic@sentice.com>
2023-11-08 17:58:19 +0100

    Intial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo" />
+      <h1>Pinia tasks</h1>
+    </header>
+
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task"/>
+      </div>
+    </div>    
+  </main>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useTaskStore } from '@/stores/TaskStore'
+import TaskDetails from '@/components/TaskDetails.vue'
+
+const taskStore = ref(useTaskStore())
+</script>
