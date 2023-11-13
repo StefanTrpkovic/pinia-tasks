61c0a39c51564311d5b4dbb02eacd4ce357b6da4
Stefan Trpkovic Sentice <stefan.trpkovic@sentice.com>
2023-11-13 12:46:20 +0100

    Final commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,56 @@
 <template>
   <main>
+    <!-- heading -->
     <header>
       <img src="./assets/pinia-logo.svg" alt="pinia logo" />
       <h1>Pinia tasks</h1>
     </header>
 
-    <div class="task-list">
-      <div v-for="task in taskStore.tasks">
-        <TaskDetails :task="task"/>
+    <!-- new task form -->
+    <div class="new-task-form">
+      <TaskForm />
+    </div>
+
+    <!-- filter -->
+    <nav class="filter">
+      <button v-on:click="filter = 'all'">All tasks</button>
+      <button v-on:click="filter = 'fav'">Fav tasks</button>
+    </nav>
+
+    <!-- loading -->
+    <div class="loading" v-if="loading">Loading tasks...</div>
+
+    <!-- task list -->
+    <div class="task-list" v-if="filter === 'all'">
+      <p>You have {{ totalCount }} tasks left to do</p>
+      <div v-for="task in tasks">
+        <TaskDetails :task="task" />
       </div>
-    </div>    
+    </div>
+    <div class="task-list" v-else>
+      <p>You have {{ favCount }} favs left to do</p>
+      <div v-for="task in favs">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
+    <button @click="taskStore.$reset">Reset store</button>
+
   </main>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { storeToRefs } from 'pinia'
 import { useTaskStore } from '@/stores/TaskStore'
 import TaskDetails from '@/components/TaskDetails.vue'
+import TaskForm from '@/components/TaskForm.vue'
 
-const taskStore = ref(useTaskStore())
+const taskStore = useTaskStore()
+const { tasks, favs, loading, totalCount, favCount } = storeToRefs(taskStore)
+taskStore.getTasks()
+
+const filter = ref('all')
+
+
 </script>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -2,10 +2,55 @@ import { defineStore } from 'pinia'
 
 export const useTaskStore = defineStore('taskStore', {
   state: () => ({
-    tasks: [
-      {id: 1, title: 'buy some milk', isFav: false},
-      {id: 2, title: 'play Gloomhaven', isFav: true}
-    ],
+    tasks: [],
+    loading: false,
     name: 'Yoshi'
-  })
+  }),
+  getters: {
+    favs: (state) => state.tasks.filter((task) => task.isFav),
+    favCount: (state) => state.tasks.reduce((p, c) => (c.isFav ? p + 1 : p), 0),
+    totalCount: (state) => state.tasks.length
+  },
+  actions: {
+    async getTasks() {
+      this.loading = true
+      const res = await fetch('http://localhost:3000/tasks')
+      const data = await res.json()
+      this.tasks = data
+      this.loading = false
+    },
+    async addTask(task) {
+      this.tasks.push(task)
+
+      await fetch('http://localhost:3000/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(task)
+      })
+    },
+    async deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id)
+      await fetch(`http://localhost:3000/tasks/${id}`, {
+        method: "DELETE"
+      })
+
+    },
+    async toggleFav(id) {
+      const task = this.tasks.find((task) => task.id === id)
+      task.isFav = !task.isFav
+
+      await fetch(`http://localhost:3000/tasks/${id}`, {
+        method: "PATCH",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          isFav: task.isFav
+        })
+      })
+
+    }
+  }
 })
